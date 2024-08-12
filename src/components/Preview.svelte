c34ec24e66f5786047f60784280bf68abddbeb05
tiendang <tiendvlp@gmail.com>
2024-08-12 22:22:59 +0700

    Design welcome email

--- src/components/Preview.svelte
+++ src/components/Preview.svelte
@@ -3,8 +3,8 @@
     export let html: String
 
     const screenSize = {
-        phone: "width: 390px; height: 844px",
-        tablet: "width: 820px; height: 1180px",
+        phone: "width: 390px; height: 844px; overflow: scroll",
+        tablet: "width: 820px; height: 1180px; overflow: scroll",
         desktop: "width: 1742px; height: 906px"
     }
 
@@ -20,9 +20,7 @@
                 <button on:click={() => selectedScreenSize = screenSize.phone} class="border border-gray-200 rounded-sm bg-gray-100 px-1"><svg width="23px" height="38px" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g fill="none" stroke="#000000" stroke-width="2"> <path d="M42 1H22c-2.2 0-4 1.8-4 4v54c0 2.2 1.8 4 4 4h20c2.2 0 4-1.8 4-4V5c0-2.2-1.8-4-4-4zM32 60h-1c-.5 0-1-.4-1-1v-1c0-.6.5-1 1-1h1c.6 0 1 .4 1 1v1c0 .6-.4 1-1 1zm12-7H20V7h24v46z"></path> </g> </g></svg></button>
             </div>
             <div class="overflow-scroll p-5 w-full flex justify-center">
-                <div style="{selectedScreenSize}" class="border rounded-lg overflow-clip bg-white m-5">
-                    {@html html}
-                </div>
+                <iframe srcdoc="{html}" style="{selectedScreenSize}" class="border rounded-lg overflow-clip bg-white m-5"/>
             </div>
         </div>
     </div>
--- src/routes/email/welcome/welcome.mail.svelte
+++ src/routes/email/welcome/welcome.mail.svelte
@@ -3,13 +3,49 @@
   export let datetime: string;
 </script>
 
-<mj-section>
-  <mj-column>
-    <mj-button
-      color="#fff"
-      background-color="#ff3e00"
-      font-family="Helvetica">
-      Hi {userName}
-    </mj-button>
-  </mj-column>
-</mj-section>
+<mj-body>
+  <mj-raw>
+    <!-- Company Header -->
+  </mj-raw>
+  <mj-section background-color="#EEE">
+    <mj-column>
+      <mj-text font-size="15px" color="#000" align="center" font-weight="200">
+          <div style="align-items: center; display: flex; justify-content: center; justify-items: center;">
+              Devlog Studio
+              <img src="https://devlogs.sgp1.cdn.digitaloceanspaces.com/public/assets/ic_devlog.png" alt="logo" style="margin-left: 5px; width: 20px; height: 20px;" />
+          </div>
+      </mj-text>
+    </mj-column>
+  </mj-section>
+  <mj-raw>
+    <!-- Image Header -->
+  </mj-raw>
+  <mj-section background-url="https://devlogs.sgp1.cdn.digitaloceanspaces.com/public/assets/gradient-background.gif" background-size="cover" background-repeat="no-repeat">
+    <mj-column width="600px">
+      <mj-text align="center" color="#fff" font-size="30px">Sharing is our key to success</mj-text>
+      <mj-button background-color="#402E7A" href="https://devlog.studio">Please visit</mj-button>
+    </mj-column>
+  </mj-section>
+  <mj-raw>
+    <!-- Intro text -->
+  </mj-raw>
+  <mj-section background-color="#fafafa">
+    <mj-column width="400px">
+      <mj-text font-size="20px" align="center">Appriciate</mj-text>
+      <mj-text align="center" font-weight="200" line-height="18px"> Thank you for your subscription, {userName}. Our primary mission is to develop a wide range of productivity software, and along the way, we are committed to giving back to the community. We begin by sharing our knowledge, and your subscription to our blog is a valuable sign of our impact. Your subscription is truly appreciated. Thank you. </mj-text>
+      <mj-button background-color="#402E7A" href="https://devlog.studio">Visit us</mj-button>
+    </mj-column>
+  </mj-section>
+  <mj-section background-color="#EEE">
+    <mj-column>
+      <mj-text align="center" color="#B4B4B8" font-weight="200">
+          Website: <a href="https://devlog.studio" style="text-decoration: underline;">
+            devlog.studio
+          </a>
+      </mj-text>
+      <mj-text align="center" color="#B4B4B8" font-weight="200">Contact: team@devlog.studio</mj-text>
+      <mj-text align="center" color="#B4B4B8" font-weight="200">Tan Binh, Ho Chi Minh, Viet Nam</mj-text>
+      <mj-text align="center" color="#B4B4B8" font-weight="200">You're receiving this email because you subscribed to the devlog.studio.</mj-text>
+    </mj-column>
+  </mj-section>
+</mj-body>
